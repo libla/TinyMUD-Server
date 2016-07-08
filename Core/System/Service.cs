11cfb15ac75c4f0a32907a0509ded96916f0b6ae
libla <libla@qq.com>
2016-07-08 10:02:23 +0800

    加入服务类

---
+++ Core/System/Service.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace TinyMUD
+{
+	public abstract class Service
+	{
+		protected Loop MainLoop;
+
+		public void Run()
+		{
+			MainLoop = Loop.Current;
+			OnInit();
+			MainLoop.Run();
+			Exit();
+		}
+
+		internal abstract void OnInit();
+		protected virtual void Exit() { }
+	}
+
+	public abstract class Service<T> : Service where T : Service<T>, new()
+	{
+		internal override void OnInit()
+		{
+			Init();
+		}
+
+		protected abstract void Init();
+
+		public static T Instance()
+		{
+			return new T();
+		}
+	}
+}
