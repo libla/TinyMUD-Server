cd264e4d3794c824e2528502ada92f7dc241c54e
libla <libla@qq.com>
2016-07-11 09:36:19 +0800

    修改启动流程，加入命令行解析

--- Startup/Main.cs
+++ Startup/Main.cs
@@ -1,45 +1,73 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
 
 namespace TinyMUD
 {
 	static class Startup
 	{
+		class Options
+		{
+			[ValueList(typeof(List<string>), MaximumElements = 1)]
+			public IList<string> Configs { get; set; }
+
+			[OptionList('i', "import", Separator = ':', DefaultValue = new string[0], HelpText = "Import Librarys.")]
+			public IList<string> Imports { get; set; }
+
+			[Option('d', "dir", DefaultValue = ".", HelpText = "File search path.")]
+			public string Directory { get; set; }
+
+			[HelpOption]
+			public string GetUsage()
+			{
+				Assembly assembly = Assembly.GetExecutingAssembly();
+				AssemblyProductAttribute product = (AssemblyProductAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyProductAttribute));
+				HelpText help = new HelpText(string.Format("{0} {1}", product.Product, assembly.GetName().Version))
+				{
+					AddDashesToOption = true,
+				};
+				help.AddOptions(this);
+				return help.ToString();
+			}
+
+		}
 		static void Main(string[] args)
 		{
-			///
-			/// 加载DLL
-			System.Reflection.Assembly.LoadFrom("Example\\SimpleServer.dll");
-			Type current = null;
-			foreach (System.Reflection.Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+			Options options = new Options();
 			{
-				if (current != null)
-					break;
-				foreach (Type type in assembly.GetTypes())
+				Parser parser = new Parser(settings =>
 				{
-					string name = type.Name;
-					if (!string.IsNullOrEmpty(type.Namespace))
-						name = type.Namespace + "." + name;
-					if (name == "TinyMUD.SimpleServer")
-					{
-						current = type;
-						break;
-					}
-				}
+					settings.CaseSensitive = false;
+					settings.IgnoreUnknownArguments = false;
+					settings.HelpWriter = System.Console.Out;
+				});
+				if (!parser.ParseArguments(args, options))
+					return;
 			}
-			///
-			Application.Startup(args);
-			Log.Add(new ConsoleLog());
-			///
-			/// 运行初始化函数
-			Action load = Delegate.CreateDelegate(typeof(Action), current, "Load") as Action;
-			load();
-			///
+			if (!string.IsNullOrEmpty(options.Directory) && options.Directory != ".")
+				Environment.CurrentDirectory = options.Directory;
+			if (options.Configs.Count > 0)
+			{
+				Config config = Config.Load(File.ReadAllBytes(options.Configs[0]));
+				string[] imports = new string[options.Imports.Count];
+				options.Imports.CopyTo(imports, 0);
+				Application.Startup(config, imports);
+			}
+			else
+			{
+				string[] imports = new string[options.Imports.Count];
+				options.Imports.CopyTo(imports, 0);
+				Application.Startup(Config.Empty, imports);
+			}
+			Console.OnInput(s =>
+			{
+				if (s == "q" || s == "quit")
+					Environment.Exit(0);
+			});
 			Loop.Current.Run();
-			///
-			/// 运行清理函数
-			Action unload = Delegate.CreateDelegate(typeof(Action), current, "Unload") as Action;
-			unload();
-			///
 			Application.Exit();
 		}
 	}
