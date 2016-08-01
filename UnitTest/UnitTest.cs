79eba2879ad27bd39ff0238be8bff20009db8d04
libla <libla@qq.com>
2016-08-02 02:12:38 +0800

    新增单元测试项目

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TinyMUD
+{
+	[TestClass]
+	public class UnitTest
+	{
+		[TestMethod]
+		public void TestMethod()
+		{
+		}
+	}
+}
