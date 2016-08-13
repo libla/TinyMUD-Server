71f6894dc3262f89ebcde800a7840faab9c95e1b
libla <libla@qq.com>
2016-08-13 14:03:40 +0800

    加入Dummy格式(纯空转，不读不写)，以对比测试protocol库的性能

---
+++ Core/Protocol/Dummy.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace TinyMUD
+{
+	public static partial class Protocol
+	{
+		public static Format Dummy()
+		{
+			return new DummyFormat();
+		}
+
+		private class DummyFormat : Format
+		{
+			#region 读取处理实现
+			public override void Skip()
+			{
+			}
+
+			protected override void ReadTable(Action<Format, int> readtype)
+			{
+			}
+
+			protected override void ReadArray(Action<Format> readarray)
+			{
+			}
+
+			protected override bool ReadBool()
+			{
+				return false;
+			}
+
+			protected override int ReadInt()
+			{
+				return 0;
+			}
+
+			protected override double ReadFloat()
+			{
+				return 0;
+			}
+
+			protected override string ReadString()
+			{
+				return "";
+			}
+
+			protected override void PrepareRead(Stream stream)
+			{
+			}
+
+			protected override void FlushRead()
+			{
+			}
+			#endregion
+
+			#region 写入处理实现
+			protected override void WriteTable(Cursor cursor)
+			{
+				bool result = cursor.MoveNext();
+				if (result)
+				{
+					cursor.Execute(this);
+					while (cursor.MoveNext())
+					{
+						cursor.Execute(this);
+					}
+				}
+			}
+
+			protected override void WriteArray(Cursor cursor)
+			{
+				bool result = cursor.MoveNext();
+				if (result)
+				{
+					cursor.Execute(this);
+					while (cursor.MoveNext())
+					{
+						cursor.Execute(this);
+					}
+				}
+			}
+
+			protected override void WriteKey(int key)
+			{
+			}
+
+			protected override void WriteBool(bool b)
+			{
+			}
+
+			protected override void WriteInt(int n)
+			{
+			}
+
+			protected override void WriteFloat(double d)
+			{
+			}
+
+			protected override void WriteString(string s)
+			{
+			}
+
+			protected override void PrepareWrite(Stream stream)
+			{
+			}
+
+			protected override void FlushWrite()
+			{
+			}
+			#endregion
+		}
+	}
+}
